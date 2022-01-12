a09f7ad6271b5562e85ed4a302e4c96ddfe7be2f
Felipe Rodriguez Esturo <feliress13@gmail.com>
2022-01-12 18:02:28 -0300

    add truffle compile on exec

--- exactly-fin-challenge/script-balance.js
+++ exactly-fin-challenge/script-balance.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const Web3 = require('web3');
+const { exec } = require("child_process");
 
 const CONTRACT_ADDRESS = '0x7Ac207b24966E3922749a337C243e3dDe6A6E99A';
 const provider = new Web3.providers.HttpProvider(
@@ -9,18 +10,20 @@ const provider = new Web3.providers.HttpProvider(
 const web3 = new Web3(provider);
 
 
+exec('truffle compile', () => {
+ const ETHPoolPath = path.resolve(__dirname, 'build', 'contracts', 'ETHPool.json');
+ const source = fs.readFileSync(ETHPoolPath, 'utf8');
 
-const ETHPoolPath = path.resolve(__dirname, 'build', 'contracts', 'ETHPool.json');
-const source = fs.readFileSync(ETHPoolPath, 'utf8');
+ const ABI = JSON.parse(source).abi;
 
-const ABI = JSON.parse(source).abi;
+ const scriptFunction = async () => {
+  const ethPoolContract = await new web3.eth.Contract(ABI, CONTRACT_ADDRESS);
+  const theoricalBalance = await ethPoolContract.methods.balance().call();
+  const realBalance = await web3.eth.getBalance(CONTRACT_ADDRESS)
+  console.log(theoricalBalance);
+  console.log(realBalance);
+ }
 
-const scriptFunction = async () => {
- const ethPoolContract = await new web3.eth.Contract(ABI, CONTRACT_ADDRESS);
- const theoricalBalance = await ethPoolContract.methods.balance().call();
- const realBalance = await web3.eth.getBalance(CONTRACT_ADDRESS)
- console.log(theoricalBalance);
- console.log(realBalance);
-}
+ scriptFunction();
+});
 
-scriptFunction();
